7d039f2efb18bf477d5ab7dd916ae2d7e1257956
Vighnesh-45 <dhotrevighnesh45@gmail.com>
2024-10-06 19:22:45 +0530

    admin login basic ui done

--- src/Admin/Alogin.jsx
+++ src/Admin/Alogin.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import "./Alogin.css"
 const Alogin = () => {
   return (
-    <div>Alogin</div>
+    <div className="login-main">
+      <div className="login-container">
+        <h2>Admin Login</h2>
+
+        {/* Login Form */}
+        <form action="">
+          <div className="login-form">
+            <div className="login-input">
+              <label>Username</label>
+              <input type="text" name="username" placeholder="Enter your username" />
+            </div>
+            
+            <div className="login-input">
+              <label>Password</label>
+              <input type="password" name="" id="" placeholder="Enter your password" />
+            </div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
